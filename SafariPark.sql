11e0d136fd5113e20c9c5c16c615821bcb8ee328
Billie Redwood <billieredwood@gmail.com>
2023-10-26 18:01:19 +0100

    Completed tasks and extensions

--- SafariPark.sql
+++ SafariPark.sql
@@ -42,16 +42,98 @@ INSERT INTO enclosures (name, capacity, closed_for_maintenance) VALUES ('Bird Pe
 -- Staff inserts
 INSERT INTO staff (name, employee_number) VALUES ('Billie the BirdKeeper', 1);
 INSERT INTO staff (name, employee_number) VALUES ('Albert the Big Cat Guy', 2);
+INSERT INTO staff (name, employee_number) VALUES ('Charlie the Turtle Man', 3);
+INSERT INTO staff (name, employee_number) VALUES ('Charlie the Turtle Woman', 4);
+INSERT INTO staff (name, employee_number) VALUES ('Charles the Old Turtle Man', 5);
 
 -- Assignment inserts
 INSERT INTO assignment (employee_id, enclosure_id, day) VALUES (1, 3, 'Monday');
 INSERT INTO assignment (employee_id, enclosure_id, day) VALUES (2, 1, 'Monday');
+INSERT INTO assignment (employee_id, enclosure_id, day) VALUES (3, 2, 'Monday');
+INSERT INTO assignment (employee_id, enclosure_id, day) VALUES (4, 2, 'Tuesday');
+INSERT INTO assignment (employee_id, enclosure_id, day) VALUES (5, 2, 'Wednesday');
 
 -- Animal inserts
 INSERT INTO animals (name, type, age, enclosure_id) VALUES ('Tony', 'Tiger', 59, 1);
+INSERT INTO animals (name, type, age, enclosure_id) VALUES ('Tibo', 'Tiger', 40, 1);
+INSERT INTO animals (name, type, age, enclosure_id) VALUES ('Tina', 'Liger', 30, 1);
 INSERT INTO animals (name, type, age, enclosure_id) VALUES ('Tommy', 'Turtle', 101, 2);
 INSERT INTO animals (name, type, age, enclosure_id) VALUES ('Polly', 'Parrot', 25, 3);
 
+-- Q1 
+-- SELECT * 
+-- FROM animals 
+-- INNER JOIN enclosures
+-- ON animals.enclosure_id = enclosures.id
+-- WHERE animals.enclosure_id = 1;
+
+-- Q2
+-- SELECT staff.name 
+-- FROM enclosures 
+-- INNER JOIN assignment
+-- ON enclosures.id = assignment.enclosure_id
+-- INNER JOIN staff
+-- ON employee_id = staff.id
+-- WHERE enclosures.name = 'Big Cats';
+
+-- extension 
+-- SELECT *
+-- FROM enclosures 
+-- INNER JOIN assignment
+-- ON enclosures.id = assignment.enclosure_id
+-- INNER JOIN staff
+-- ON employee_id = staff.id
+-- WHERE enclosures.closed_for_maintenance = true AND day = 'Tuesday';
+
+-- to find the enclosure of the oldest animals
+-- SELECT *
+-- FROM animals
+-- ORDER BY age DESC;
+
+-- SELECT enclosures.name
+-- FROM enclosures
+-- INNER JOIN animals
+-- ON enclosures.id = animals.enclosure_id
+-- WHERE age = '101'
+
+
+
+-- number of diff. animal types a keeper has been assigned to work with.
+-- SELECT COUNT(DISTINCT type)
+-- FROM enclosures
+-- INNER JOIN animals
+-- ON enclosures.id = animals.enclosure_id
+-- INNER JOIN assignment
+-- ON assignment.enclosure_id = enclosures.id
+-- INNER JOIN staff
+-- ON employee_id = staff.id
+-- WHERE staff.name = 'Albert the Big Cat Guy';
+
+
+-- number of diff keepers assigned to an enclosure
+-- SELECT COUNT(DISTINCT staff.name)
+-- FROM enclosures
+-- INNER JOIN animals
+-- ON enclosures.id = animals.enclosure_id
+-- INNER JOIN assignment
+-- ON assignment.enclosure_id = enclosures.id
+-- INNER JOIN staff
+-- ON employee_id = staff.id
+-- WHERE enclosures.name = 'Shell People';
+
+
+SELECT enclosures.name, animals.name
+FROM enclosures
+INNER JOIN animals
+ON enclosures.id = animals.enclosure_id
+INNER JOIN assignment
+ON assignment.enclosure_id = enclosures.id
+INNER JOIN staff
+ON employee_id = staff.id
+WHERE enclosures.name = 'Big Cats' AND animals.name != 'Tony'
+
+
+-- The names of staff working in enclosures which are closed for maintenance.
 
 
 
